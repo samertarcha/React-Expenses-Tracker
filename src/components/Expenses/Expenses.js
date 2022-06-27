645df20c32453060506341fc12d7164155d90aea
samertarcha <samertarcha@outlook.com>
2022-06-27 08:51:21 +0300

    add files to commit

---
+++ src/components/Expenses/Expenses.js
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+
+import Card from "../UI/Card";
+import ExpensesFilter from "./ExpensesFilter";
+import ExpensesList from "./ExpensesList";
+import ExpensesChart from "./ExpensesChart";
+import "./Expenses.css";
+
+const Expenses = (props) => {
+	const [filteredYear, setFilteredYear] = useState("2020");
+
+	const FilterChangeHandler = (selectedYear) => {
+		setFilteredYear(selectedYear);
+	};
+
+	const filteredExpenses = props.items.filter((expense) => {
+		return expense.date.getFullYear().toString() === filteredYear;
+	});
+
+	return (
+		<div>
+			<li>
+				<Card className="expenses">
+					<ExpensesFilter selected={filteredYear} onChangeFilter={FilterChangeHandler} />
+					<ExpensesChart expenses={filteredExpenses} />
+					<ExpensesList items={filteredExpenses} />
+				</Card>
+			</li>
+		</div>
+	);
+};
+
+export default Expenses;
